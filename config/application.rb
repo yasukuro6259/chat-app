a0afc1ce1b0bc24d822380e034354aa94d986465
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-09-07 01:42:37 +0900

    空のメッセージが送信できないバリデーションの設定

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module ChatApp
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
