2ed29f8885f7a7729cb45550379596a1a90e2404
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-09-06 23:24:16 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def create
+    @room = Room.find(params[:room_id]) #createアクションで得られた情報ということ？ 上の@roomが更新されたということ？
+    @message = @room.messages.new(message_params)
+    @message.save
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
