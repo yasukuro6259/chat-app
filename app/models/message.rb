000d70b5bb0b09de08115a7f740feab2a6996bbd
yasukuro6259 <64528846+yasukuro6259@users.noreply.github.com>
2020-09-07 15:52:09 +0900

    messageモデルのバリデーションを修正しました。

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
-  
-  validates :content, presence: true
   has_one_attached :image #Messagesテーブルの各レコードと画像ファイルを管理するimageカラム?の紐付けに成功?
   validates :content, presence: true, unless: :was_attached? #unlessオプション 特定の条件(下で定義したwas_attachedメソッド)でバリデーションを行わない
 
